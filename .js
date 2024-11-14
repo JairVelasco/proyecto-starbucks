87b377031b03cd0be3aff11987119f3d90fcce13
Jair <jairvelasco27082001@gmail.com>
2024-11-14 10:54:19 -0600

    pequeña pagina de starbucks

---
+++ .js
@@ -0,0 +1,5 @@
+// JavaScript para el menú desplegable
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+// Toggle el menú en pant
